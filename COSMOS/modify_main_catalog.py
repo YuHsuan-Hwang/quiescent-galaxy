1b4b3c644b3fa5670bf112cd32375fa665d16d77
yuhsuan <r07222045@ntu.edu.tw>
2019-10-19 17:12:27 +0800

    new version

--- COSMOS/modify_main_catalog.py
+++ COSMOS/modify_main_catalog.py
@@ -9,7 +9,7 @@ Created on Mon Feb 25 13:34:58 2019
 # packages
 # =============================================================================
 import time
-from astropy.table import Table
+from astropy.table import Table, hstack
 
 # =============================================================================
 # functions
@@ -17,40 +17,145 @@ from astropy.table import Table
 def ReadCatalog(catalog):
     return Table.read(catalog, hdu=1)
 
-def AddColumns():
+def AddColumns(t,data,columns):
     for i in range(len(columns)):
         t[columns[i]] = data[columns[i]]
+
+def SimplifyMainCat():
+    ###set catalog name
+    inputcatalog = "COSMOS2015_Laigle+_v1.1"
+    catalog = inputcatalog+".fits"
+    
+    ###set columns
+    columns = [ "ALPHA_J2000", "DELTA_J2000", "NUMBER", "MNUV", "MR", "MJ", "PHOTOZ","ZQ",\
+               "V_MAGERR_APER3", "ip_MAGERR_APER3", "J_MAGERR_APER3", "Ks_MAGERR_APER3",\
+               "MASS_MED", "TYPE", "CLASS", "Ks_MAG_APER3",\
+               "MASS_BEST","FLAG_HJMCC","FLAG_PETER","FLAG_COSMOS","FLAG_DEEP","FLAG_SHALLOW"]
+    
+    ###read catalog
+    data = ReadCatalog(catalog)
+    
+    ###add columns
+    t = Table()
+    AddColumns(t,data,columns)
+    
+    ###output fits file
+    t.write(inputcatalog+'_simple.fits')
+    return
+
+def SimplifyAGNCat():
+    ###set catalog name
+    inputcatalog = "cos_agn"
+    catalog = inputcatalog+".fits"
+    
+    ###set columns
+    columns = [ "agn_c17b","agn_xxx" ]
+    
+    ###read catalog
+    data = ReadCatalog(catalog)
+    
+    ###add columns
+    t = Table()
+    AddColumns(t,data,columns)
+    
+    ###output fits file
+    t.write("COSMOS2015_"+inputcatalog+'_simple.fits')
+    return
+
+def SimplifyAGNcaCat():
+    ###set catalog name
+    inputcatalog = "ca_all_iragns"
+    catalog = inputcatalog+".fits"
+    
+    ###set columns
+    columns = [ "IRAGN" ]
+    
+    ###read catalog
+    data = ReadCatalog(catalog)
+    
+    ###add columns
+    t = Table()
+    AddColumns(t,data,columns)
+    
+    ###output fits file
+    t.write("COSMOS2015_"+inputcatalog+'_simple.fits')
+    return
+
+def MergingMainCat():
+    
+    ###set catalog name
+    inputcatalog = "COSMOS2015_"
+    cats = ["Laigle+_v1.1_simple_z",\
+            "24micron","3GHz","ALMA10","lensing","850wide","450narrow",\
+            "cos_agn_simple","ca_all_iragns_simple"]
+    
+    ###read tables
+    tables = []
+    for i in range(len(cats)):
+        tables.append(Table.read(inputcatalog+cats[i]+".fits",hdu=1))
+    
+    ###merage tables
+    output_cat = hstack([tables[0],tables[1]], join_type='exact')
+    for i in range(len(cats)-2):
+        output_cat = hstack([output_cat,tables[i+2]], join_type='exact')
+    
+    ###output
+    output_cat.write("COSMOS2015_merged.fits")
+    return
+
+def MergingColumnRedshift():
+    
+    ###set catalog name
+    inputcatalog = "COSMOS2015_Laigle+_v1.1_simple"
+    catalog = inputcatalog+".fits"
+    
+    SOURCE_COLUMN = "REDSHIFT"
+    
+    ###read catalog
+    data = ReadCatalog(catalog)
+    
+    SOURCE = [-999.99]*len( data )
+    
+    print len(data)
+    
+    print "loop start"
+    for i in range( len(data) ):
+        if data["PHOTOZ"][i]>9.9:
+            SOURCE[i] = data["ZQ"][i]
+        else:
+            SOURCE[i] = data["PHOTOZ"][i]
+        if i==10000:
+            time2 = time.time()
+            print 'current time :', (time2-time1)/60.0 , 'min'
         
+    data[SOURCE_COLUMN] = SOURCE
+    
+    ###output
+    data.write("COSMOS2015_Laigle+_v1.1_simple_z.fits")
+    #print SOURCE
+    return
+
 # =============================================================================
 # main code
 # =============================================================================
+
 time1 = time.time()
 
-###set catalog name
-inputcatalog = "COSMOS2015_Laigle+_v1.1_5+2band_2agn_9cat_ALMA4"
-#"COSMOS2015_Laigle+_v1.1_850sources"
-catalog = inputcatalog+".fits"
+# ===== Simplify =====
+#SimplifyMainCat()
 
-###set columns
-columns = [ "ALPHA_J2000", "DELTA_J2000", "NUMBER", "MNUV", "MR", "MJ", "PHOTOZ",\
-           "V_MAGERR_APER3", "ip_MAGERR_APER3", "J_MAGERR_APER3", "Ks_MAGERR_APER3",\
-           "MASS_MED", "TYPE", "CLASS", "Ks_MAG_APER3",\
-           "850SOURCE","850NARROW","450NARROW","24MICRON","3GHZ", \
-           "IR_AGN_ALL","IR_AGN_MIR",\
-           "Xray_AGN","MIR_AGN","SED_AGN","Quiescent_MLAGN","SFG","Clean_SFG",\
-           "HLAGN","MLAGN","Radio_excess","450NARROW_SIMPLE","850WIDE_SIMPLE",\
-           "MASS_BEST","ALMA_05","ALMA_10","850WIDE_ALMA","850WIDE_ALMA_10",\
-           "FLAG_HJMCC","FLAG_PETER","FLAG_COSMOS","FLAG_DEEP","FLAG_SHALLOW"]
+# ===== Simplify AGN catalog from yu-yen =====
+#cos_agn.fits
+#SimplifyAGNCat()
 
-###read catalog
-data = ReadCatalog(catalog)
+#ca_all_iragns.fits
+#SimplifyAGNcaCat()
 
-###add columns
-t = Table()
-AddColumns()
+# ===== Merging two redshift in COSMOS2015 =====
+MergingColumnRedshift()
 
-###output fits file
-t.write(inputcatalog+'_simple.fits')
+# ===== Merging =====
+MergingMainCat()
 
 time2 = time.time()
-print 'done! time =', time2-time1 , 'sec'
+print 'done! time =', (time2-time1)/60.0 , 'min'
